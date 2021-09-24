6bfac8aa7c1294edcfc3404b2450b9fb09208fe2
Joonwoo Kwon <pioneers@snu.ac.kr>
2021-09-24 21:26:06 +0900

    Update fitting.py

--- storm_analysis/sa_library/fitting.py
+++ storm_analysis/sa_library/fitting.py
@@ -880,7 +880,6 @@ class PeakFinderFitter(object):
 
         self.peak_finder.newImage(image)
         self.peak_fitter.newImage(image)
-        print("movie shape : ", movie_reader.getFrame().shape)
 
         for i in range(self.peak_finder.iterations):
 
